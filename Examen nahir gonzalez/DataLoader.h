a9c31844ac6fcbb5cf19244be5dcf9e728eb68fb
nahirgonzalezcarrillo <23-10181@usb.ve>
2026-06-15 07:12:02 -0400

    Delete Examen nahir gonzalez directory

--- Examen nahir gonzalez/DataLoader.h
+++
@@ -1,9 +0,0 @@
-#ifndef DATALOADER_H
-#define DATALOADER_H
-#include "types.h"
-
-class DataLoader : public IDataLoader {
-public:
-    std::vector<Coord_3D> load(const std::string& filename) override;
-};
-#endif
